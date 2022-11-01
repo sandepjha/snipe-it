5b37c3644372d5901c3190c014a59f486e22be94
sandepjha <sandeepjha741@gmail.com>
2022-11-01 07:47:55 +0530

    Initial commit

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v6.0.11',
+  'full_app_version' => 'v6.0.11 - build 8758-g61fa0bda4',
+  'build_version' => '8758',
+  'prerelease_version' => '',
+  'hash_version' => 'g61fa0bda4',
+  'full_hash' => 'v6.0.11-145-g61fa0bda4',
+  'branch' => 'master',
+);
